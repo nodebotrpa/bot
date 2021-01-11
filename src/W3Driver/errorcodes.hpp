3d7424fdfa3e90c9ae51001e5c54b8bd8f185f94
NodeBot <68096949+nodebotrpa@users.noreply.github.com>
2021-01-11 22:33:09 +0300

    Add files via upload

---
+++ src/W3Driver/errorcodes.hpp
@@ -0,0 +1,12 @@
+#ifndef ERRORCODES_HPP
+#define ERRORCODES_HPP
+
+const String WEB_NO_DRIVER = "Driver is not running";
+const String WEB_NO_BROWSER = "Browser is not running";
+const String WEB_NO_ACTIVE_BROWSER = "No running browser";
+const String WEB_INVALID_SESSION = "Invalid session name";
+const String WEB_TIMEOUT = "Connection timeout, check timeout parameters";
+const String WEB_NOT_VISIBLE = "Element is not visible";
+const String WEB_NOT_ENABLED = "Element is not enabled";
+
+#endif
